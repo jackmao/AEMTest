fdba5ed334c8de3cd0ec8636bb6517ca075e9488
jackmao <jack.mao@cnsdc01.pwc.com>
2015-04-02 17:27:36 +0800

    test

---
+++ aemtest/bundle/src/main/java/com/pwc/tomashu/aemtest/HelloService.java
@@ -0,0 +1,13 @@
+package com.pwc.tomashu.aemtest;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
